ed08a59b35a7ac4c5403be589797805716dac83e
michaldrozdz <mich0232@gmail.com>
2022-11-29 01:44:13 +0100

    Add outputs

---
+++ outputs.tf
@@ -0,0 +1,11 @@
+output "arn" {
+  value = aws_lambda_function.this.arn
+}
+
+output "invoke_arn" {
+  value = aws_lambda_function.this.invoke_arn
+}
+
+output "version" {
+  value = aws_lambda_function.this.version
+}
